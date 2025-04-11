60e11a85ee93830fa06164e68fc7434c066b889f
RandomHashTags <imrandomhashtags@gmail.com>
2025-04-10 20:23:56 -0500

    added day of week unit tests

---
+++ Tests/swift-staticdatetimeTests/TestsDayOfWeek.swift
@@ -0,0 +1,31 @@
+//
+//  TestsDayOfWeek.swift
+//
+//
+//  Created by Evan Anderson on 5/31/24.
+//
+
+@testable import StaticDateTimes
+import Testing
+
+struct TestsDayOfWeek {
+    @Test func test_distance() {
+        #expect(UInt8.sunday.dowDistance(to: UInt8.monday) == 1)
+        #expect(UInt8.sunday.dowDistance(to: UInt8.saturday) == 6)
+        #expect(UInt8.sunday.dowDistance(to: UInt8.sunday) == 7)
+
+        #expect(UInt8.saturday.dowDistance(to: UInt8.sunday) == 1)
+        #expect(UInt8.saturday.dowDistance(to: UInt8.friday) == 6)
+        #expect(UInt8.saturday.dowDistance(to: UInt8.saturday) == 7)
+    }
+    @Test func test_addition() {
+        #expect(UInt8.sunday.add(1) == UInt8.monday)
+        #expect(UInt8.sunday.add(6) == UInt8.saturday)
+        #expect(UInt8.sunday.add(7) == UInt8.sunday)
+
+        #expect(UInt8.saturday.add(1) == UInt8.sunday)
+        #expect(UInt8.saturday.add(2) == UInt8.monday)
+        #expect(UInt8.saturday.add(6) == UInt8.friday)
+        #expect(UInt8.saturday.add(7) == UInt8.saturday)
+    }
+}
